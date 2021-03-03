64f8941ae1668971ed89675a4b1a21fbbab3a7bc
ckn3 <33248323+ckn3@users.noreply.github.com>
2021-03-03 18:16:32 +0800

    Add files via upload

--- test_SalinasA/auxiliary/plot_abundance1.m
+++ test_SalinasA/auxiliary/plot_abundance1.m
@@ -1,40 +1,35 @@
-function y = plot_abundance(X)
+function y = plot_abundance1(X)
 
 figure;
-subplot(2,4,1)
+subplot(2,3,1)
 imagesc(X(:,:,1))
 axis equal off
 title('EM1')
 
-subplot(2,4,2)
+subplot(2,3,2)
 imagesc(X(:,:,2))
 axis equal off
 title('EM2')
 
-subplot(2,4,3)
+subplot(2,3,3)
 imagesc(X(:,:,3))
 axis equal off
 title('EM3')
 
-subplot(2,4,4)
+subplot(2,3,4)
 imagesc(X(:,:,4))
 axis equal off
 title('EM4')
 
-subplot(2,4,5)
+subplot(2,3,5)
 imagesc(X(:,:,5))
 axis equal off
 title('EM5')
 
-subplot(2,4,6)
+subplot(2,3,6)
 imagesc(X(:,:,6))
 axis equal off
 title('EM6')
-
-subplot(2,4,7)
-imagesc(X(:,:,7))
-axis equal off
-title('EM7')
 y = 1;
 
 end
