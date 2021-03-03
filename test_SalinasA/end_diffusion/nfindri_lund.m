a511542603bb89443e567258d5c70c239d3abb99
ckn3 <33248323+ckn3@users.noreply.github.com>
2021-03-03 18:20:36 +0800

    Add files via upload

--- test_SalinasA/end_diffusion/nfindri_lund.m
+++ test_SalinasA/end_diffusion/nfindri_lund.m
@@ -1,4 +1,4 @@
-function [acc,labels,t] = atgp_lund(X, k, h, w, G, T, Y, flag)
+function [acc,labels,t] = nfindri_lund(X, k, h, w, G, T, Y, flag)
 
 % Input:
 %      - X: p*N matrix, each column represent a pixel
@@ -16,7 +16,6 @@ function [acc,labels,t] = atgp_lund(X, k, h, w, G, T, Y, flag)
 %      - t: diffision time
 
 
-
 acc = zeros(length(T),1);
 t = acc;
 
@@ -24,31 +23,37 @@ t = acc;
 
 if flag == 0
     
+    
     labels = zeros(length(Y),length(T));
-    
-    for i = 1:length(T)
-        [ATGP,~] = EIA_ATGP(X,k);
-        [X_atgp] = hyperNnls(X, ATGP);
-        X_atgp = reshape(X_atgp', h, w, k);
-    
-        
 
-        p = compute_purity(X_atgp,83,86,k);
+    for i = 1:length(T)
+        [NFINDR,~] = EIA_NFINDR(X,k+2,200);
+        [X_nfindr] = hyperNnls(X, NFINDR);
         
+        X_nfindr = reshape(X_nfindr', h, w, k+2);
+        X_nfindr = X_nfindr(:,:,3:k+2);
+        
+        p = compute_purity(X_nfindr,83,86,k);
         [acc(i),labels(:,i),~,t(i),~] = LUND_labels(X, Y, p, G, T(i), h, w, k, 0);
     end
+    
+    
     
 elseif flag == 1
     
     labels = zeros(length(Y(Y~=1)),length(T));
-    for i = 1:length(T)
-        [ATGP,~] = EIA_ATGP(X(:,Y~=1),k);
-        [X_atgp] = hyperNnls(X, ATGP);
-        X_atgp = reshape(X_atgp', h, w, k);
     
-        p = compute_purity(X_atgp,83,86,k);
+    for i = 1:length(T)
+        [NFINDR,~] = EIA_NFINDR1(X(:,Y~=1),k+2,200);
+        [X_nfindr] = hyperNnls(X, NFINDR);
+        X_nfindr = reshape(X_nfindr', h, w, k+2);
+        X_nfindr = X_nfindr(:,:,3:k+2);
+        
+        p = compute_purity(X_nfindr,83,86,k);
         [acc(i),labels(:,i),~,t(i),~] = LUND_labels(X, Y, p, G, T(i), h, w, k, 1);
     end
+    
+
 end
 
 
